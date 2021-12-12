da92064884824084fbb2496cd17ac96b42332f2a
rezaSemyari70 <rezasemyari1991@gmail.com>
2021-12-12 15:23:06 +0330

    add panel purchases ui and dictionary of this page and edit style of switch language

--- src/main/router.js
+++ src/main/router.js
@@ -4,6 +4,7 @@ import App from '../App';
 import ConfirmMobile from '../pages/ConfirmMobile/ConfirmMobile';
 import Home from '../pages/Home.jsx/Home';
 import Login from '../pages/Login/Login';
+import PanelMyPurchases from '../pages/PanelMyPurchases/PanelMyPurchases';
 import Panelprofile from '../pages/PanelProfile/PanelProfile';
 import SetPassword from '../pages/RecoveryPassword/RecoveryPassword';
 import Signup from '../pages/Singup/Signup';
@@ -30,6 +31,7 @@ const RouterConfig = (props) => {
             <Routes>
                 <Route path="panel" animate={true}>
                     <Route index path="profile" element={<Panelprofile />} />
+                    <Route index path="purchases" element={<PanelMyPurchases />} />
                     
                 </Route>
             </Routes>
