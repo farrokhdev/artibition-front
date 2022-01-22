91c506bcac533f597d22e407a729844b7a36f5ee
ehsan mashali <ehsanmashali222.78@gmail.com>
2022-01-22 10:28:32 +0330

    api call in panle order

--- src/pages/PanelOrders/PanelOrders.jsx
+++ src/pages/PanelOrders/PanelOrders.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import BasketFooterPanel from '../../components/BasketFooterPanel/BasketFooterPanel';
 import HeaderPanel from '../../components/HeaderPanel/HeaderPanel';
 import SidebarPanel from '../../components/SidebarPanel/SidebarPanel';
@@ -6,62 +6,133 @@ import { t } from 'i18next';
 import { Pagination } from 'antd';
 
 import TableOrders from './TableOrders';
+import apiServices from '../../utils/api.services';
+import { ORDER_SELERS } from '../../utils/index'
+import queryString from 'query-string';
 
 function PanelOrders() {
+
+    const [loading, setLoading] = useState(false);
+    const [orderList, setOrderList] = useState("");
+    const [suggestionsCount, setSuggestionsCount] = useState("");
+    const [params, setParams] = useState({
+        page: 1,
+        status: "",
+    });
+
+
+    // Receive the list of orders
+    const getIgetOrdersLisItems = () => {
+        setLoading(true)
+        apiServices.get(ORDER_SELERS, queryString.stringify(params))
+            .then(resp => {
+                setLoading(false)
+                setOrderList(resp.data.data.results)
+                setSuggestionsCount(resp.data.data.count)
+            })
+            .catch(err => {
+                setLoading(false)
+                console.error(err);
+            })
+    }
+
+
+    // This section is for filtering the status of orders
+    const handleStatus = (e) => {
+        setParams({
+            ...params, status: e
+        })
+    }
+
+    // Shows us the position of the page we are on
+    const handeSelectPage = (e) => {
+        setParams({
+            ...params, page: e
+        })
+    }
+
+
+    useEffect(() => {
+        getIgetOrdersLisItems()
+    }, [params])
+
+
     return (
         <>
-      <HeaderPanel t={t} />
-      <div className="panel-style margin-top-20">
-        <SidebarPanel />
-        <div className="custom-container">
+            <HeaderPanel t={t} />
+            <div className="panel-style margin-top-20">
+                <SidebarPanel />
+                <div className="custom-container">
 
-        <div className="box artistpanel-5 dir">
+                    <div className="box artistpanel-5 dir">
 
-            <div className="public-header mrgb32">
-                <h2 className=" default-title text-dir">{t("content-panel-dashboard.tables.orders")}</h2>
+                        <div className="public-header mrgb32">
+                            <h2 className=" default-title text-dir">{t("content-panel-dashboard.tables.orders")}</h2>
+                        </div>
+                        <div className="row box-dir-reverse text-dir">
+                            <div className="col-12 col-sm-6 col-xl-3 px-0">
+                                <label className="container-radio">{t("content-panel-orders.filters.all")}
+                                    <input
+                                        id="tab-11" data-bs-toggle="tab"
+                                        data-bs-target="#profiletab1"
+                                        role="tab" aria-controls="profiletab1"
+                                        aria-selected="true"
+
+                                        type="radio" name="radio" onClick={() => handleStatus("")} />
+                                    <span className="checkmark-radio"></span>
+                                </label>
+                            </div>
+                            <div className="col-12 col-sm-6 col-xl-3 px-0">
+                                <label className="container-radio">{t("content-panel-orders.filters.to_artibition")}
+                                    <input
+                                        id="tab-21" data-bs-toggle="tab"
+                                        data-bs-target="#profiletab2"
+                                        role="tab" aria-controls="profiletab2"
+                                        aria-selected="false"
+
+                                        type="radio" name="radio" onClick={() => handleStatus("pending")} />
+                                    <span className="checkmark-radio"></span>
+                                </label>
+                            </div>
+                            <div className="col-12 col-sm-6 col-xl-3 px-0">
+                                <label className="container-radio">{t("content-panel-orders.filters.artibition_to_customer")}
+                                    <input
+                                        id="tab-31" data-bs-toggle="tab"
+                                        data-bs-target="#profiletab3"
+                                        role="tab" aria-controls="profiletab3"
+                                        aria-selected="false"
+
+                                        type="radio" name="radio" onClick={() => handleStatus("sent_to_buyer")} />
+                                    <span className="checkmark-radio"></span>
+                                </label>
+                            </div>
+                            <div className="col-12 col-sm-6 col-xl-3 px-0">
+                                <label className="container-radio">{t("content-panel-orders.filters.delivered")}
+                                    <input type="radio" name="radio" onClick={() => handleStatus("delivered")} />
+                                    <span className="checkmark-radio"></span>
+                                </label>
+                            </div>
+                        </div>
+
+
+                        <TableOrders id orderList={orderList} />
+
+
+                        <div className=" row-pagination">
+                            <Pagination
+                                defaultCurrent={1}
+                                total={suggestionsCount}
+                                defaultPageSize={10}
+                                onChange={(e) => handeSelectPage(e)} />
+                        </div>
+
+                    </div>
+
+
+                </div>
+                <BasketFooterPanel />
             </div>
-            <div className="row box-dir-reverse text-dir">
-                <div className="col-12 col-sm-6 col-xl-3 px-0">
-                    <label className="container-radio">{t("content-panel-orders.filters.all")}
-                        <input type="radio" checked="checked" name="radio"/>
-                        <span className="checkmark-radio"></span>
-                    </label>
-                </div>
-                <div className="col-12 col-sm-6 col-xl-3 px-0">
-                    <label className="container-radio">{t("content-panel-orders.filters.to_artibition")}
-                        <input type="radio" name="radio"/>
-                        <span className="checkmark-radio"></span>
-                    </label>
-                </div>
-                <div className="col-12 col-sm-6 col-xl-3 px-0">
-                    <label className="container-radio">{t("content-panel-orders.filters.artibition_to_customer")}
-                        <input type="radio" name="radio"/>
-                        <span className="checkmark-radio"></span>
-                    </label>
-                </div>
-                <div className="col-12 col-sm-6 col-xl-3 px-0">
-                    <label className="container-radio">{t("content-panel-orders.filters.delivered")}
-                        <input type="radio" name="radio"/>
-                        <span className="checkmark-radio"></span>
-                    </label>
-                </div>
-            </div>
-
-
-            <TableOrders/>
-            
-
-            <div className=" row-pagination">
-                <Pagination  total={50} />
-            </div>
-
-        </div>
-
-    
-        </div>
-        <BasketFooterPanel />
-      </div>
-    </>
+        </>
     )
 }
 
--- src/pages/PanelOrders/TableOrders.jsx
+++ src/pages/PanelOrders/TableOrders.jsx
@@ -1,123 +1,69 @@
 import React from 'react';
 import { t } from 'i18next';
+import { isAccept } from '../../utils/converToPersian';
 
-import artwork1 from '../../assets/img/artworks/artwork-1.jpg';
+function TableOrders({ orderList }) {
 
-function TableOrders() {
+
+
+    // Offers a list of orders based on the status filter
+    const SuggestOrderList = () => {
+        return (
+            orderList && orderList.length >= 1 ? orderList.map((item, key) => {
+                return (
+                    <tr key={key} className='pending-state'>
+                        <td data-label={t("content-panel-dashboard.tables.row")} className="persian-num">{key + 1}</td>
+                        <td data-label={t("content-panel-dashboard.tables.image")}>
+                            <img
+                                src={item?.product_item?.product?.medias[0]?.exact_url}
+                                width="1776" height="1776" alt=""
+                                className="img-responsive "
+                            />
+                        </td>
+                        <td data-label={t("content-panel-dashboard.tables.code")} className="persian-num">{item?.id}</td>
+                        <td data-label={t("content-panel-dashboard.tables.edition")} className="persian-num">{item?.product_item?.edition_number}</td>
+                        <td data-label={t("content-panel-dashboard.tables.price")} className="persian-num">{item?.product_item?.reserved_toman_price}</td>
+                        <td data-label={t("content-panel-dashboard.tables.discount")} className="persian-num">{item?.discount}</td>
+                        <td data-label={t("content-panel-dashboard.tables.paid")} className="persian-num">{item?.toman_price}</td>
+                        <td data-label={t("content-panel-dashboard.tables.status")} className="persian-num status text-dir text-md-center">{isAccept(item?.status).title}</td>
+                        <td data-label={t("content-panel-dashboard.tables.details")} className="">{item?.description} </td>
+                        <td data-label={t("content-panel-dashboard.tables.action")}>
+                            <button className="btn-outline-blue" type="button" data-toggle="modal"
+                                data-target="#modal-replied-price">{t("content-panel-dashboard.tables.veiw")}
+                            </button>
+                        </td>
+                    </tr>
+                )
+            }) : ""
+        )
+    }
+
+
     return (
         <div className="d-flex box-dir-reverse ">
-                <div collapse className="table-responsive ">
-                    <table className="table  wallet-table  dir">
-                                        <thead>
-                                            <tr>
-                                                <td>{t("content-panel-dashboard.tables.row")}</td>
-                                                <td>{t("content-panel-dashboard.tables.image")}</td>
-                                                <td>{t("content-panel-dashboard.tables.code")}</td>
-                                                <td className='min-width-td-2'>{t("content-panel-dashboard.tables.edition")}</td>
-                                                <td className='min-width-td-4'>{t("content-panel-dashboard.tables.price")}</td>
-                                                <td className='min-width-td-1'>{t("content-panel-dashboard.tables.discount")}</td>
-                                                <td className='min-width-td-4'>{t("content-panel-dashboard.tables.paid")}</td>
-                                                <td className="min-width-td-6">{t("content-panel-dashboard.tables.status")}</td>
-                                                <td>{t("content-panel-dashboard.tables.details")}</td>
-                                                <td>{t("content-panel-dashboard.tables.action")}</td>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr className='pending-state'>
-                                                <td data-label={t("content-panel-dashboard.tables.row")} className="persian-num">1</td>
-                                                <td data-label={t("content-panel-dashboard.tables.image")}>
-                                                    <img 
-                                                        src={artwork1} 
-                                                        width="1776" height="1776" alt=""
-                                                        className="img-responsive "
-                                                    />
-                                                </td>
-                                                <td data-label={t("content-panel-dashboard.tables.code")} className="persian-num">۱۲۷۵</td>
-                                                <td data-label={t("content-panel-dashboard.tables.edition")} className="persian-num">۲</td>
-                                                <td data-label={t("content-panel-dashboard.tables.price")} className="persian-num">۴,۰۰۰,۰۰۰</td>
-                                                <td data-label={t("content-panel-dashboard.tables.discount")} className="persian-num">۰</td>
-                                                <td data-label={t("content-panel-dashboard.tables.paid")} className="persian-num">۴,۰۰۰,۰۰۰</td>
-                                                <td data-label={t("content-panel-dashboard.tables.status")} className="persian-num status text-dir text-md-center">منتظر ارسال اثر به آرتیبیشن</td>
-                                                <td data-label={t("content-panel-dashboard.tables.details")} className="">جزییات </td>
-                                                <td data-label={t("content-panel-dashboard.tables.action")}>
-                                                    <button className="btn-outline-blue" type="button" data-toggle="modal"
-                                                            data-target="#modal-replied-price">{t("content-panel-dashboard.tables.veiw")}
-                                                    </button>
-                                                </td>
-                                            </tr>
+            <div collapse className="table-responsive " >
+                <table className="table  wallet-table  dir">
+                    <thead>
+                        <tr>
+                            <td>{t("content-panel-dashboard.tables.row")}</td>
+                            <td>{t("content-panel-dashboard.tables.image")}</td>
+                            <td>{t("content-panel-dashboard.tables.code")}</td>
+                            <td className='min-width-td-2'>{t("content-panel-dashboard.tables.edition")}</td>
+                            <td className='min-width-td-4'>{t("content-panel-dashboard.tables.price")}</td>
+                            <td className='min-width-td-1'>{t("content-panel-dashboard.tables.discount")}</td>
+                            <td className='min-width-td-4'>{t("content-panel-dashboard.tables.paid")}</td>
+                            <td className="min-width-td-6">{t("content-panel-dashboard.tables.status")}</td>
+                            <td>{t("content-panel-dashboard.tables.details")}</td>
+                            <td>{t("content-panel-dashboard.tables.action")}</td>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-                                            <tr className='pending-state'>
-                                            <td data-label={t("content-panel-dashboard.tables.row")} className="persian-num">2</td>
-                                                <td data-label={t("content-panel-dashboard.tables.image")}>
-                                                    <img 
-                                                        src={artwork1} 
-                                                        width="1776" height="1776" alt=""
-                                                        className="img-responsive "
-                                                    />
-                                                </td>
-                                                <td data-label={t("content-panel-dashboard.tables.code")} className="persian-num">۱۲۷۵</td>
-                                                <td data-label={t("content-panel-dashboard.tables.edition")} className="persian-num">۲</td>
-                                                <td data-label={t("content-panel-dashboard.tables.price")} className="persian-num">۴,۰۰۰,۰۰۰</td>
-                                                <td data-label={t("content-panel-dashboard.tables.discount")} className="persian-num">۰</td>
-                                                <td data-label={t("content-panel-dashboard.tables.paid")} className="persian-num">۴,۰۰۰,۰۰۰</td>
-                                                <td data-label={t("content-panel-dashboard.tables.status")} className="persian-num status text-dir text-md-center">منتظر ارسال اثر به آرتیبیشن</td>
-                                                <td data-label={t("content-panel-dashboard.tables.details")} className="">جزییات </td>
-                                                <td data-label={t("content-panel-dashboard.tables.action")}>
-                                                    <button className="btn-outline-blue" type="button" data-toggle="modal"
-                                                            data-target="#modal-replied-price">{t("content-panel-dashboard.tables.veiw")}
-                                                    </button>
-                                                </td>
-                                            </tr>
-
-                                            <tr className='delivered-state'>
-                                            <td data-label={t("content-panel-dashboard.tables.row")} className="persian-num">3</td>
-                                                <td data-label={t("content-panel-dashboard.tables.image")}>
-                                                    <img 
-                                                        src={artwork1} 
-                                                        width="1776" height="1776" alt=""
-                                                        className="img-responsive "
-                                                    />
-                                                </td>
-                                                <td data-label={t("content-panel-dashboard.tables.code")} className="persian-num">۱۲۷۵</td>
-                                                <td data-label={t("content-panel-dashboard.tables.edition")} className="persian-num">۲</td>
-                                                <td data-label={t("content-panel-dashboard.tables.price")} className="persian-num">۴,۰۰۰,۰۰۰</td>
-                                                <td data-label={t("content-panel-dashboard.tables.discount")} className="persian-num">۰</td>
-                                                <td data-label={t("content-panel-dashboard.tables.paid")} className="persian-num">۴,۰۰۰,۰۰۰</td>
-                                                <td data-label={t("content-panel-dashboard.tables.status")} className="persian-num status text-dir text-md-center">منتظر ارسال اثر به آرتیبیشن</td>
-                                                <td data-label={t("content-panel-dashboard.tables.details")} className="">جزییات </td>
-                                                <td data-label={t("content-panel-dashboard.tables.action")}>
-                                                    <button className="btn-outline-blue" type="button" data-toggle="modal"
-                                                            data-target="#modal-replied-price">{t("content-panel-dashboard.tables.veiw")}
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            <tr className ='pending-state'>
-                                            <td data-label={t("content-panel-dashboard.tables.row")} className="persian-num">4</td>
-                                                <td data-label={t("content-panel-dashboard.tables.image")}>
-                                                    <img 
-                                                        src={artwork1} 
-                                                        width="1776" height="1776" alt=""
-                                                        className="img-responsive justify-custom"
-                                                    />
-                                                </td>
-                                                <td data-label={t("content-panel-dashboard.tables.code")} className="persian-num">۱۲۷۵</td>
-                                                <td data-label={t("content-panel-dashboard.tables.edition")} className="persian-num">۲</td>
-                                                <td data-label={t("content-panel-dashboard.tables.price")} className="persian-num">۴,۰۰۰,۰۰۰</td>
-                                                <td data-label={t("content-panel-dashboard.tables.discount")} className="persian-num">۰</td>
-                                                <td data-label={t("content-panel-dashboard.tables.paid")} className="persian-num">۴,۰۰۰,۰۰۰</td>
-                                                <td data-label={t("content-panel-dashboard.tables.status")} className="persian-num status text-dir text-md-center">منتظر ارسال اثر به آرتیبیشن</td>
-                                                <td data-label={t("content-panel-dashboard.tables.details")} className="">جزییات </td>
-                                                <td data-label={t("content-panel-dashboard.tables.action")}>
-                                                    <button className="btn-outline-blue" type="button" data-toggle="modal"
-                                                            data-target="#modal-replied-price">{t("content-panel-dashboard.tables.veiw")}
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                            
-                                        </tbody>
-                    </table>
-                </div>
+                        <SuggestOrderList />
+                    </tbody>
+                </table>
             </div>
+        </div>
     )
 }
 
