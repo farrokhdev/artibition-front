8ac4176cdeb475af3362a5f476b8ba63f8006d15
Amin Mousavi <amin.mousavi71@gmail.com>
2022-01-23 14:18:51 +0000

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://185.226.116.16/api/v1";
+export const BASE_URL = "http://api.artibition.gallery/api/v1";
 // export const BASE_URL = "http://192.168.0.113:8000/api/v1";
 
 
