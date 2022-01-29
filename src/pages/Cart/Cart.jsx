cb671a3d768319fcff6f9660726d4ff4f500ef50
f.hadadi <fhadadimoj@gmail.com>
2022-01-29 17:44:26 +0330

    daily commit working on cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,104 +1,135 @@
-import React from 'react';
-import Footer from '../../components/Footer/Footer';
+import React, { useEffect, useState } from "react";
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
-import Menu from '../../components/Menu/Menu';
-import classnames from 'classnames';
-import { GetLanguage } from '../../utils/utils';
+import Menu from "../../components/Menu/Menu";
+import classnames from "classnames";
+import { GetLanguage } from "../../utils/utils";
 
-import empty from '../../assets/img/Empty.svg';
+import empty from "../../assets/img/Empty.svg";
 
-
-import { t } from 'i18next';
-import CartItem from './CartItem';
-import CartItemDisable from './CartItemDisable';
-import ArthibitionProperties from '../../components/ArthibitionProperies/ArthibitionProperties';
-
-
-
-
-
+import { t } from "i18next";
+import CartItem from "./CartItem";
+import CartItemDisable from "./CartItemDisable";
+import ArthibitionProperties from "../../components/ArthibitionProperies/ArthibitionProperties";
+import apiServices from "../../utils/api.services";
+import { CART_ME } from "../../utils";
+import { useNavigate } from "react-router-dom";
 
 function Cart() {
-    return (
-        <>
-            <div className="container mx-auto px-0 w-100 bg-white">
-                <Header />
-                <Menu />
+  const [product_items, setProduct_items] = useState();
+  const navigate = useNavigate();
+  const getData = () => {
+    apiServices
+      .get(CART_ME, "")
+      .then((res) => {
+        console.log("res", res?.data?.data);
+        setProduct_items(res?.data?.data?.product_items);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  const removeSingleProduct = (id, edition_number) => {
+    console.log("remove me", id, edition_number);
+  };
+  return (
+    <>
+      <div className="container mx-auto px-0 w-100 bg-white">
+        <Header />
+        <Menu />
 
-                {/* ------------- EMPTY STATE -------------- */}
-                <div className="container dir">
-                    <div className="emptystate">
-                        <img src={empty} width="103" height="98" alt="" />
-                        <h3>{t("cart.empty.title")}</h3>
-                        <p>
-                            {t("cart.empty.description")}
-                        </p>
-                        <button type="button" className="btn btn-artworksimilar">{t("cart.empty.btn")}</button>
-                    </div>
-                </div>
-                {/* ------------- EMPTY STATE -------------- */}
-
-
-
-
-
-                {/* ------------- FULL STATE -------------- */}
-                <div className="container dir">
-                    <div className="public-header">
-                        <div className="col-xs-12">
-                            <h2 className="default-title pull-dir">{t("cart.full.cart")}</h2>
-                            <div className="basket-notification pull-dir ">
-                                <span className="persian-num">2</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="basket-list">
-                        <CartItem />
-                        <CartItem />
-                        <CartItem />
-                        <CartItemDisable />
-
-
-                        <div className="clearfix"></div>
-                        <div className="row">
-                            <div className={classnames("", {
-                                "col-md-4 col-md-offset-8": GetLanguage() === 'fa-IR',
-                                "col-md-5 col-md-offset-7": GetLanguage() === 'en-US'
-                            })}>
-                                <div className="basket-total">
-                                    <div className="basket-price">
-                                        <div className="clearfix"></div>
-                                        <div className="price-row d-flex">
-                                            <div className="col-xs-5 text-dir">{t("cart.full.total_price")}</div>
-                                            <div className="col-xs-7">
-                                                <div className="basket-pricestyle pull-dir-rev">
-                                                    <span className="persian-num">۲,۸۰۰,۰۰۰</span>
-                                                    <span>{t("cart.full.price_unit")}</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-xs-12">
-                                            <span className="greencolor">
-                                                {t("cart.full.less_price_received_separately")}
-                                            </span>
-                                            <button type="button" className="btn btn-continue">{t("cart.full.next")}</button>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <ArthibitionProperties />
-                </div>
-                {/* ------------- FULL STATE -------------- */}
-
-
+        {/* ------------- EMPTY STATE -------------- */}
+        {product_items?.length < 1 && (
+          <div className="container dir">
+            <div className="emptystate">
+              <img src={empty} width="103" height="98" alt="" />
+              <h3>{t("cart.empty.title")}</h3>
+              <p>{t("cart.empty.description")}</p>
+              <button
+                type="button"
+                className="btn btn-artworksimilar"
+                onClick={() => navigate("/site/artworks")}
+              >
+                {t("cart.empty.btn")}
+              </button>
             </div>
+          </div>
+        )}
+        {/* ------------- EMPTY STATE -------------- */}
 
-            <Footer />
-        </>
-    )
+        {/* ------------- FULL STATE -------------- */}
+        <div className="container dir">
+          {product_items?.length > 0 && (
+            <div className="public-header">
+              <div className="col-xs-12">
+                <h2 className="default-title pull-dir">
+                  {t("cart.full.cart")}
+                </h2>
+                <div className="basket-notification pull-dir ">
+                  <span className="persian-num">2</span>
+                </div>
+              </div>
+            </div>
+          )}
+          <div className="basket-list">
+            {product_items?.map((product_item) => {
+              return (
+                <CartItem
+                  removeProduct={removeSingleProduct}
+                  singleProduct={product_item}
+                />
+              );
+            })}
+            {/* <CartItem />
+            <CartItem />
+            <CartItemDisable /> */}
+
+            <div className="clearfix"></div>
+            <div className="row">
+              <div
+                className={classnames("", {
+                  "col-md-4 col-md-offset-8": GetLanguage() === "fa-IR",
+                  "col-md-5 col-md-offset-7": GetLanguage() === "en-US",
+                })}
+              >
+                {/* <div className="basket-total">
+                  <div className="basket-price">
+                    <div className="clearfix"></div>
+                    <div className="price-row d-flex">
+                      <div className="col-xs-5 text-dir">
+                        {t("cart.full.total_price")}
+                      </div>
+                      <div className="col-xs-7">
+                        <div className="basket-pricestyle pull-dir-rev">
+                          <span className="persian-num">۲,۸۰۰,۰۰۰</span>
+                          <span>{t("cart.full.price_unit")}</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-xs-12">
+                      <span className="greencolor">
+                        {t("cart.full.less_price_received_separately")}
+                      </span>
+                      <button type="button" className="btn btn-continue">
+                        {t("cart.full.next")}
+                      </button>
+                    </div>
+                  </div>
+                </div> */}
+              </div>
+            </div>
+          </div>
+          {/* <ArthibitionProperties /> */}
+        </div>
+        {/* ------------- FULL STATE -------------- */}
+      </div>
+
+      <Footer />
+    </>
+  );
 }
 
 export default Cart;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,5 @@
 export const BASE_URL = "https://api.artibition.gallery/api/v1";
 
-
-
 export const COOKIE_EXPIRES = 1;
 export const LOGIN = "/account/login/";
 export const REGISTER = "/account/register/";
@@ -9,37 +7,44 @@ export const VERIFY = "/account/verify/";
 export const PROFILE = "/account/profile/";
 export const OPT = "/account/sendotp/";
 export const RECOVER_PASSWORD = "/account/recover-password/";
-export const ORDER_BUYERS = "/orders/buyers/"
-export const ORDER_SELERS = "/orders/sellers/"
-export const GALLERY_PANEL_DASHBOARD = (galleryId) => { return (`/gallery/${galleryId}/dashboard/`) }
+export const ORDER_BUYERS = "/orders/buyers/";
+export const ORDER_SELERS = "/orders/sellers/";
+export const GALLERY_PANEL_DASHBOARD = (galleryId) => {
+  return `/gallery/${galleryId}/dashboard/`;
+};
 export const PROMOTIONS = "/events/promotions/";
-export const EXHIBITION = (galleryId) => { return (`/${galleryId}/exhibition/`) };
+export const EXHIBITION = (galleryId) => {
+  return `/${galleryId}/exhibition/`;
+};
 export const PRODUCTS_ME = "/products/me/";
 export const PRODUCTS = "/products/";
 export const PRE_UPLOAD = "/core/upload/";
 export const CORE_CATEGORIS = "/core/category/";
-export const  PRODUCTS_CATEGORIES ="/products/categories/";
-export const SUBJECTS_CATEGORISE = id =>`/products/categories/${id}/subjects/`;
-export const TECHNIQUS_CATEGORIES = id => `/products/categories/${id}/techniques/`;
-export const MATERIALS_CATEGORIES = id => `/products/categories/${id}/materials/`;
-export const SOCIAL_NETWORK_COLLECTIONS = "/social-network/collections/"
+export const PRODUCTS_CATEGORIES = "/products/categories/";
+export const SUBJECTS_CATEGORISE = (id) =>
+  `/products/categories/${id}/subjects/`;
+export const TECHNIQUS_CATEGORIES = (id) =>
+  `/products/categories/${id}/techniques/`;
+export const MATERIALS_CATEGORIES = (id) =>
+  `/products/categories/${id}/materials/`;
+export const SOCIAL_NETWORK_COLLECTIONS = "/social-network/collections/";
 export const GALLERY_LIST = "/gallery/";
-export const GALLERY = id => `/gallery/${id}/`
-export const GALLERY_EXHIBITION = id => `/${id}/exhibition/`
-export const GALLERY_ARTISTS = id => `/${id}/artists/`
-export const GALLERY_CONTENT = id => `/${id}/content/`
+export const GALLERY = (id) => `/gallery/${id}/`;
+export const GALLERY_EXHIBITION = (id) => `/${id}/exhibition/`;
+export const GALLERY_ARTISTS = (id) => `/${id}/artists/`;
+export const GALLERY_CONTENT = (id) => `/${id}/content/`;
 export const ARTIST = "/artist/";
 export const ARTIST_PRODUCTS = "/products/";
 export const ARTIST_CATEGORY = "/social-network/collections/";
-export const ARTIST_PROFILE = id => `/artist/${id}/`;
+export const ARTIST_PROFILE = (id) => `/artist/${id}/`;
 export const ARTIST_CONTENT = "/artist/content/";
 export const ARTIST_EXHIBITION = "/artist/exhibition/";
 export const WALLET = "/accounting/wallet/me/";
 export const WALLET_TRANSACTIONS = "/accounting/wallet-transactions/";
 export const GALLERY_FOLLOW = "/social-network/follow/";
-export const ARTIST_BY_GALLERY = id => `/${id}/artists/define/`
+export const ARTIST_BY_GALLERY = (id) => `/${id}/artists/define/`;
 export const ACCOUNT_PROFILE = "/account/profile/";
-export const PRODUCT_DETAIL = id => `/products/${id}/`
+export const PRODUCT_DETAIL = (id) => `/products/${id}/`;
 export const MESSAGES_ASSERTION = "/message/assertion/";
 export const MESSAGES_INBOX = "/message/inbox/";
-
+export const CART_ME = "/carts/me/";
