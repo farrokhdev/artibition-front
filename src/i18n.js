7e8076ee9a63ba91207fa39672436992c4816b96
amnmoj-117 <md.ramezanpoor@gmail.com>
2022-02-01 17:25:13 +0330

    default lang fa

--- src/i18n.js
+++ src/i18n.js
@@ -21,7 +21,6 @@ i18n
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
         fallbackLng: 'fa',
-        lng: 'fa-IR',
         debug: false,
 
         interpolation: {
