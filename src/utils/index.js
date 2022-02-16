b130286659aadf3e64f0813c0f5bd940344aab60
mohsen <mohsenamahade1000@gmail.com>
2022-02-16 15:34:54 +0330

    .

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 
-// export const BASE_URL = " http://192.168.0.23:8808/api/v1";
+export const BASE_URL = " http://192.168.0.23:8808/api/v1";
 // export const BASE_URL = " http://192.168.0.100:8000/api/v1";
 // export const BASE_URL = " http://192.168.0.113:8000/api/v1";
 // export const BASE_URL = " http://192.168.0.145:8000/api/v1";
@@ -9,7 +9,7 @@
 // export const BASE_URL = " http://local.devmoj.ir/api/v1";
 
 
-export const BASE_URL  = "https://api.artibition.gallery/api/v1";
+// export const BASE_URL  = "https://api.artibition.gallery/api/v1";
 
 export const COOKIE_EXPIRES = 1;
 export const LOGIN = "/account/login/";
