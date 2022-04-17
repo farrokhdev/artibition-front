2524e9cf1a161465ec0a64dab6dfd6d0231f93ea
f.hadadi <fhadadimoj@gmail.com>
2022-04-17 16:41:30 +0430

    bug fix in exhibition carousel

---
+++ src/components/ExhibitionComponents/ExhibitionsPage/ExhibitionCarousel.jsx
@@ -0,0 +1,151 @@
+import { isNil } from "lodash";
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { Link, useNavigate } from "react-router-dom";
+const ExhibitionDetail = (exhibition) => {
+  const { t, i18n } = useTranslation();
+  const navigate = useNavigate();
+  return (
+    <Link
+      to="/site/exhibitionDetail"
+      className="event-row "
+      state={{
+        id: exhibition?.id,
+        gallery_id: exhibition?.gallery?.id,
+      }}
+      //   onClick={() =>
+      //     navigate("/site/exhibitionDetail", {
+      //       state: {
+      //         id: exhibition?.id,
+      //         gallery_id: exhibition?.gallery?.id,
+      //       },
+      //       replace: false,
+      //     })
+      //   }
+    >
+      <div className="event-img pull-right">
+        <img
+          src={exhibition?.poster?.[0]?.exact_url}
+          alt="رویداد آرتیبیشن"
+          className="img-responsive"
+          style={{
+            maxWidth: "60px",
+            maxHeight: "60px",
+            minHeight: "60px",
+            minWidth: "60px",
+          }}
+        />
+      </div>
+      <div className="event-body">
+        <h6 className="event-title">
+          {i18n.language === "fa-IR"
+            ? exhibition?.translations?.fa?.name
+            : exhibition?.translations?.en?.name}
+        </h6>
+        <p className="event-txt">
+          {i18n.language === "fa-IR"
+            ? exhibition?.translations?.fa?.statement
+            : exhibition?.translations?.en?.statement}
+        </p>
+      </div>
+    </Link>
+  );
+};
+const ExhibitionCarousel = ({ exhibitionsList }) => {
+  const [indicators, setIndicators] = useState([]);
+  useEffect(() => {
+    const getCarouselIndicators = () => {
+      const size =
+        Math.floor(exhibitionsList?.length) % 3 === 0
+          ? exhibitionsList?.length / 3
+          : exhibitionsList?.length / 3 + 1;
+
+      const indicators = [];
+      for (let i = 1; i < Math.floor(size); i++) {
+        indicators.push(
+          <li data-target="#myCarousel" data-slide-to={`${i}`}></li>
+        );
+      }
+      return indicators;
+    };
+    setIndicators(getCarouselIndicators());
+  }, []);
+
+  const chunk = (arr, size) =>
+    arr.reduce(
+      (acc, e, i) => (
+        i % size ? acc[acc.length - 1].push(e) : acc.push([e]), acc
+      ),
+      []
+    );
+  return (
+    <div id="myCarousel" className="carousel slide" data-ride="carousel">
+      <div className="carousel-inner">
+        {!isNil(exhibitionsList) &&
+          chunk(exhibitionsList, 3).map((innerListExhibitions, index) => {
+            return (
+              <div
+                className={`${
+                  index === 0 ? "carousel-item active" : "carousel-item"
+                }`}
+              >
+                {innerListExhibitions?.map((exhibition) => {
+                  return <ExhibitionDetail {...exhibition} />;
+                })}
+              </div>
+            );
+          })}
+      </div>
+      {/* <div className="carousel-inner">
+                  {!isNil(exhibitionsList) &&
+                    new Array(Math.ceil(exhibitionsList?.length / 3))
+                      .fill()
+                      .map((_) => exhibitionsList?.splice(0, 3))
+                      .map((innerListExhibitions, index) => {
+                        return (
+                          <div
+                            className={`${
+                              index === 3 ? "carousel-item" : "carousel-item"
+                            }`}
+                          >
+                            {innerListExhibitions?.map((exhibition) => {
+                              return <ExhibitionDetail {...exhibition} />;
+                            })}
+                          </div>
+                        );
+                      })}
+                </div> */}
+
+      <div className="event-control">
+        <ol className="carousel-indicators">
+          <li
+            data-target="#myCarousel"
+            data-slide-to="0"
+            className="active"
+          ></li>
+          {indicators}
+        </ol>
+        {/* {getCarouselIndicators()} */}
+        {/* <ol className="carousel-indicators">
+          <li
+            data-target="#myCarousel"
+            data-slide-to="0"
+            className="active"
+          ></li>
+
+          {exhibitionsList?.length > 2 && (
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+          )}
+          {exhibitionsList?.length > 5 && (
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+          )}
+          {exhibitionsList?.length > 8 && (
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+          )}
+        </ol> */}
+      </div>
+    </div>
+  );
+};
+
+export default ExhibitionCarousel;
--- src/components/ExhibitionComponents/ExhibitionsPage/ExhibitionsBanner.jsx
+++ src/components/ExhibitionComponents/ExhibitionsPage/ExhibitionsBanner.jsx
@@ -1,7 +1,5 @@
-import { isNil } from "lodash";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
 import { EXHIBITION_LIST } from "../../../utils";
 import apiServices from "../../../utils/api.services";
 import queryString from "query-string";
@@ -10,11 +8,14 @@ import persian from "react-date-object/calendars/persian";
 import gregorian from "react-date-object/calendars/gregorian";
 import persian_fa from "react-date-object/locales/persian_fa";
 import gregorian_en from "react-date-object/locales/gregorian_en";
+import moment from "moment-jalaali";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import ExhibitionCarousel from "./ExhibitionCarousel";
 const ExhibitionsBanner = () => {
   const { t, i18n } = useTranslation();
-  const navigate = useNavigate();
   const [exhibitions, setExhibitions] = useState([]);
   const [selectedDates, setSelectedDates] = useState([]);
+
   const date = selectedDates.map(
     (item) => item.format("dddd DD MMMM YYYY")
     // item.convert(gregorian, gregorian_en).format("YYYY-MMMM-DD hh:mm:ss dddd")
@@ -22,7 +23,13 @@ const ExhibitionsBanner = () => {
 
   const getData = () => {
     apiServices
-      .get(EXHIBITION_LIST, queryString.stringify({ page_size: 12 }))
+      .get(
+        EXHIBITION_LIST,
+        queryString.stringify({
+          page_size: 1000,
+          active_exhibitions: moment().format("YYYY-MM-DD"),
+        })
+      )
       .then((res) => {
         setExhibitions(res.data.data);
       })
@@ -30,48 +37,10 @@ const ExhibitionsBanner = () => {
         console.log(err);
       });
   };
+
   useEffect(() => {
     getData();
   }, []);
-  const ExhibitionDetail = (exhibition) => {
-    return (
-      <a
-        href="#"
-        className="event-row "
-        onClick={() =>
-          navigate("/site/exhibitionDetail", {
-            state: {
-              id: exhibition?.id,
-              gallery_id: exhibition?.gallery?.id,
-            },
-            replace: false,
-          })
-        }
-      >
-        <div className="event-img pull-right">
-          <img
-            src={exhibition?.poster?.[0]?.exact_url}
-            height="240"
-            width="240"
-            alt="رویداد آرتیبیشن"
-            className="img-responsive"
-          />
-        </div>
-        <div className="event-body">
-          <h6 className="event-title">
-            {i18n.language === "fa-IR"
-              ? exhibition?.translations?.fa?.name
-              : exhibition?.translations?.en?.name}
-          </h6>
-          <p className="event-txt">
-            {i18n.language === "fa-IR"
-              ? exhibition?.translations?.fa?.statement
-              : exhibition?.translations?.en?.statement}
-          </p>
-        </div>
-      </a>
-    );
-  };
 
   return (
     <div className="gallery-banner">
@@ -105,64 +74,12 @@ const ExhibitionsBanner = () => {
                 />
               </div>
             </div>
-            <div className="col-md-6 hidden-sm hidden-xs">
-              <h3 className="event-title">{t("exhibitions-page.events")}</h3>
-              <div
-                id="myCarousel"
-                className="carousel slide"
-                data-ride="carousel"
-              >
-                <div className="carousel-inner">
-                  {!isNil(exhibitions?.results) &&
-                    new Array(Math.ceil(exhibitions?.results?.length / 3))
-                      .fill()
-                      .map((_) => exhibitions?.results?.splice(0, 3))
-                      .map((innerListExhibitions, index) => {
-                        return (
-                          <div
-                            className={`${
-                              index === 3
-                                ? "carousel-item active"
-                                : "carousel-item"
-                            }`}
-                          >
-                            {innerListExhibitions?.map((exhibition) => {
-                              return <ExhibitionDetail {...exhibition} />;
-                            })}
-                          </div>
-                        );
-                      })}
-                </div>
-                <div class="controls-top">
-                  <a
-                    class="btn-floating"
-                    href="#multi-item-example"
-                    data-slide="prev"
-                  >
-                    <i class="fa fa-chevron-left"></i>
-                  </a>
-                  <a
-                    class="btn-floating"
-                    href="#multi-item-example"
-                    data-slide="next"
-                  >
-                    <i class="fa fa-chevron-right"></i>
-                  </a>
-                </div>
-                <div className="event-control">
-                  <ol className="carousel-indicators">
-                    <li
-                      data-target="#myCarousel"
-                      data-slide-to="0"
-                      className="active"
-                    ></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                    <li data-target="#myCarousel" data-slide-to="3"></li>
-                  </ol>
-                </div>
+            {exhibitions?.results?.length > 0 && (
+              <div className="col-md-6 hidden-sm hidden-xs">
+                <h3 className="event-title">{t("exhibitions-page.events")}</h3>
+                <ExhibitionCarousel exhibitionsList={exhibitions?.results} />
               </div>
-            </div>
+            )}
           </div>
         </div>
       </div>
